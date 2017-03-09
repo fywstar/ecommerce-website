18a3af2b2127c0eabaa01188fbe87b90cf59e3a5
billyang <bill.yang@immotor.com>
2017-03-09 16:32:20 +0800

    add swagger ui for rest interface show

--- site/src/main/java/com/immotor/api/RestApiMvcConfiguration.java
+++ site/src/main/java/com/immotor/api/RestApiMvcConfiguration.java
@@ -1,14 +1,39 @@
 package com.immotor.api;
 
 import org.broadleafcommerce.core.web.api.BroadleafRestApiMvcConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
+@EnableSwagger2
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.immotor.api")
 public class RestApiMvcConfiguration extends BroadleafRestApiMvcConfiguration {
-    
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.immotor.api"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring 中使用Swagger2构建RESTful APIs")
+                .termsOfServiceUrl("http://immotor.com")
+                .contact("bill yang")
+                .version("1.1")
+                .build();
+    }
 }
